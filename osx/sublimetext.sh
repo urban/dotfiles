db4e6ee4abff4f9b79523ca73edb93fdca0d5b93
Urban Faubion <urban.faubion@gmail.com>
2015-06-11 11:12:49 +0200

    cleanup

--- osx/sublimetext.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-###############################################################################
-# Sublime Text                                                                #
-###############################################################################
-
-# Install Sublime Text settings
-cp -r init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
