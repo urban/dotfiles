71482d426e056a73bd4849b9d8fdb3ac7b883882
Urban Faubion <urban.faubion@gmail.com>
2015-08-12 10:59:44 +0200

    Updated default theme for terminal and set Option key as Meta key.

--- osx/terminal.sh
+++ osx/terminal.sh
@@ -3,26 +3,24 @@
 # Terminal & iTerm 2                                                          #
 ###############################################################################
 
+# Shell opens with: /bin/bash
+defaults write com.apple.Terminal Shell -string "/bin/bash"
+
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Use a modified version of the Solarized Dark theme by default in Terminal.app
-TERM_PROFILE='Solarized Dark xterm-256color';
-CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
-if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
-  open "${HOME}/init/${TERM_PROFILE}.terminal";
-  sleep 1; # Wait a bit to make sure the theme is loaded
-  defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
-  defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
-fi;
+# Set Solarized "Solarized Dark xterm-256color" as the default
+TERM_PROFILE='Solarized Dark xterm-256color'
+default wirte com.apple.Terminal 'Startup Window Settings' -string "${TERM_PROFILE}"
+default write com.apple.Terminal 'Default Window Settings' -string "${TERM_PROFILE}"
+
+# Use Option key as Meta key
+defaults write com.apple.terminal useOptionAsMetakey -bool true
 
 # Enable “focus follows mouse” for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it without clicking first
-#defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true
-
-# Install the Solarized Dark theme for iTerm
-open "${HOME}/init/Solarized Dark.itermcolors"
+defaults write com.apple.terminal FocusFollowsMouse -bool true
+defaults write org.x.X11 wm_ffm -bool true
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
